7be7a2034310c12158f10a5ee074d395a9c40b33
c6401 <nc6401+gthb@gmail.com>
2023-01-14 23:09:11 +0000

    Remove archive

--- archive/.bashrc.d/aliases.sh
+++
@@ -1,9 +0,0 @@
-alias dc='docker-compose'
-alias pb='ansible-playbook'
-alias g='git'
-alias fd='fdfind'
-alias beep="paplay /usr/share/sounds/sound-icons/capital"
-alias ssh='echo xxh?; ssh'
-alias rmf='/usr/bin/rm'
-alias rm='echo trash-put?; sleep 5; rm'
-
