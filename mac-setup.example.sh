487989ad4cb0186e4f4f9fbda30514dc60ab2941
c6401 <nc6401+gh@gmail.com>
2023-01-31 16:49:35 +0000

    Update mac-setup.example.sh

--- mac-setup.example.sh
+++ mac-setup.example.sh
@@ -1,10 +1,17 @@
 
 git config user.name 'c6401'
 git config user.email 'nc6401+gh@gmail.com'
+
 xargs brew install < mac-brew-apps.txt
 xargs brew install --cask < mac-brew-cask-apps.txt
+
 stow git-aliases
 stow git-shortaliases
 git config --global --unset-all include.path
 git config --global --add include.path .gitconfig.d/shortaliases.config
 git config --global --add include.path .gitconfig.d/aliases.config
+
+code --install-extension stuart.unique-window-colors
+
+stow doom-autosave
+./bin/bin/blockinfile doom-configd.blockinfile ~/.doom.d/config.el
