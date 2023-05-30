500742ba64422ddbc12f587053a096e2ad8f00b5
c6401 <nc6401+gthb@gmail.com>
2023-05-30 23:57:43 +0100

    Update 00_xdg_home.sh

--- xdg-home/.profile.d/00_xdg_home.sh
+++ xdg-home/.profile.d/00_xdg_home.sh
@@ -1,7 +1,8 @@
-export XDG_DATA_HOME="$HOME/.local/share"
-export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+
 export ATOM_HOME="$XDG_DATA_HOME/atom"
 export HISTFILE="$XDG_STATE_HOME/bash/history"
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
