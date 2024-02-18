42cccaa00e3377799fd84bd4eaafca7d9f2b9d69
c6401 <c6401+gthb@gmail.com>
2024-02-18 13:26:39 +0000

    Add Mac-specific Git configurations and aliases

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,6 +1,14 @@
 brew install stow
 mkdir -p ~/.profile.d
-touch ~/.profile.d/noop.sh
 stow --adopt base-zsh
 stow --adopt unix-bin-path
 stow --adopt unix-pyenv
+stow --adopt mac-doom-autosave
+
+# gitconfig
+mkdir -p ~/.gitconfig.d
+stow unix-git-config-d
+git config --global include.path ~/.gitconfig.d/reconfigure
+stow unix-git-aliases
+stow unix-git-base
+git reconfigure
