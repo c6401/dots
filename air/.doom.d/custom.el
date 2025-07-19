ed9876563bd936a867b667952785f261cc1cb00e
c6401 <c6401+gthb@gmail.com>
2025-07-19 17:24:31 +0100

    Update org-agenda-files to include open.org path

--- air/.doom.d/custom.el
+++ air/.doom.d/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(org-agenda-files
-   '("/Volumes/swap/tasks/tasks/data.org" "/Volumes/swap/tasks/tasks/tasks.org")))
+   '("/Volumes/swap/tasks/tasks/open.org" "/Volumes/swap/tasks/tasks/data.org" "/Volumes/swap/tasks/tasks/tasks.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
