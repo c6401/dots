8bd76e1db476caf637a5040077d0d02b1cc9a1ae
c6401 <c6401+gthb@gmail.com>
2025-08-12 22:57:31 +0100

    Add function to change directory using yazi with temporary cwd file

--- air/.profile.d/aliases.sh
+++ air/.profile.d/aliases.sh
@@ -13,3 +13,10 @@ alias xcat='/bin/cat'
 alias z='zoxide'
 alias xcd='/bin/cd'
 alias py='python'
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
