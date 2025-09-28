10a38a04ad9037f468f6f0543ee83441c6425aeb
c6401 <nc6401+gthb@gmail.com>
2025-09-28 17:56:31 +0100

    Refactor common.sh to properly set environment variables for CDPATH, XDG_CONFIG_HOME, and GIT_CONFIG_GLOBAL

--- air/.profile.d/common.sh
+++ air/.profile.d/common.sh
@@ -1,4 +1,9 @@
-export CDPATH="/Volumes/swap/tasks"
+# bash
+export CDPATH="$HOME:/Volumes/swap/tasks"
+export XDG_CONFIG_HOME="$HOME/.config"
 setopt HIST_IGNORE_SPACE
 HISTSIZE=10000
 HISTFILESIZE=10000
+
+# git
+export GIT_CONFIG_GLOBAL="$HOME/.config/git/config"
