40e6b5708e97d3374b27888db109170e240011ff
c6401 <nc6401+gthb@gmail.com>
2025-11-22 18:19:21 +0000

    Update common.sh

--- air/.profile.d/common.sh
+++ air/.profile.d/common.sh
@@ -1,5 +1,5 @@
 # bash
-export CDPATH="$HOME:/Volumes/swap/tasks:/Volumes/swap:/Volumes/swap/Sync/projects"
+export CDPATH="$HOME:/Volumes/swap:/Volumes/swap/Sync:/Volumes/swap/tasks:/Volumes/Swap/Sync/projects"
 export XDG_CONFIG_HOME="$HOME/.config"
 setopt HIST_IGNORE_SPACE
 HISTSIZE=10000
