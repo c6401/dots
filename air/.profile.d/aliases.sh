e9c62a288354492c751f16b63bf6603ceb082db4
c6401 <nc6401+gthb@gmail.com>
2026-01-05 06:53:31 +0000

    Add function for executing Python code and improve existing function formatting

--- air/.profile.d/aliases.sh
+++ air/.profile.d/aliases.sh
@@ -17,8 +17,15 @@ alias katamari='nvim /Volumes/swap/Sync/katamari/'
 alias gemma='ollama run gemma3'
 alias proofread='ollama run gemma3 please proofread: "$(pbpaste)" | tee >(pbcopy)'
 
-function y() {local tmp="$(mktemp -t "yazi-cwd.XX")" cwd
-yazi "$@" --cwd-file="$tmp"
-IFS= read -r -d '' cwd <"$tmp"
-builtin cd -- "$cwd"
-rm -f -- "$tmp"}
+function px() {
+  // python execute
+  uv run python -c "i=type('_',(),{'__getattr__':lambda s,m: __import__(m)})(); x='' if i.sys.stdin.isatty() else i.sys.stdin.read(); print($*)"
+}
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd <"$tmp"
+  builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
